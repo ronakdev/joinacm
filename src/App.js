87bba358df2e217ebc32d7f4b16d4f1b21be207f
Ronak Shah <ronakshahultimate@gmail.com>
2019-10-26 11:07:04 -0700

    added firebase

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Map from "./components/Map"
+import firebase from "./util/firebase"
 
 class App extends React.Component{
   constructor(props) {
@@ -10,6 +11,12 @@ class App extends React.Component{
   }
 
   render() {
+    firebase.database().ref("spawnLocation").set({
+      updatedAt: Date.now(),
+      x: this.state.x,
+      y: this.state.y
+    })
+    
     return (
       <div className="App">
         <Map parent={this}/>
