6f8c84e654160d982750845fb2227ee175c899fa
Ronak Shah <ronakshahultimate@gmail.com>
2019-10-26 11:09:57 -0700

    made build for gh-pages

---
+++ public/precache-manifest.1a3cd15abd34a951e6048e8674943d05.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "847b090b8552fd0efb7322c66e1d1ae5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9e2e51ad1176c0191482",
+    "url": "/static/css/main.cfda8877.chunk.css"
+  },
+  {
+    "revision": "ab976a9cc76b882332e5",
+    "url": "/static/js/2.8453407d.chunk.js"
+  },
+  {
+    "revision": "9e2e51ad1176c0191482",
+    "url": "/static/js/main.ead4f2f9.chunk.js"
+  },
+  {
+    "revision": "9b63dc71e60756823f97",
+    "url": "/static/js/runtime-main.16b519db.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
