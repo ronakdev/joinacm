463161981ff70092d79da956cac4c377ab98ed44
Ronak Shah <ronakshahultimate@gmail.com>
2019-10-27 05:19:07 -0700

    oh shit windows

--- src/components/Map/index.jsx
+++ src/components/Map/index.jsx
@@ -2,10 +2,87 @@ import React, { Component } from "react";
 import { Stage, Layer, Rect, Text } from "react-konva";
 import Konva from "konva";
 import BackgroundParticles from "../Particles";
-
+import {sendSpawn} from "../../util/firebase";
 import "./style.less";
 
 class ColoredRect extends React.Component {
+  state = {
+    color: "grey"
+  };
+
+  setColor = () => {
+    // this.setState({
+    //   color: Konva.Util.getRandomColor()
+    // });  
+  }
+
+
+  render() {
+    return (
+      <Rect
+        width={550}
+        height={550}
+        opacity="0.5"
+        fill={this.state.color}
+        shadowBlur={5}
+        onClick={(e) => { this.setColor() } }
+        ref={this.props.innerRef}
+      />
+    );
+  }
+}
+
+export default class Map extends Component {
+
+  constructor(props) {
+    super(props)
+    this.rect = React.createRef();
+    this.state = {
+      zombies: []
+    }
+  }
+
+  render() {
+    // Stage is a div container
+    // Layer is actual canvas element (so you may have several canvases in the stage)
+    // And then we have canvas shapes inside the Layer
+    console.log(this.state.zombies)
+    return (
+      <div style={{ position: "relative", }} onClick={(e) => {
+        let x = e.clientX - 510
+        let y = e.clientY - 30
+        
+        let id = sendSpawn(x, y) ;
+
+        let array = this.state.zombies
+        array.push({
+          x: x,
+          y: y,
+          id: id
+        })
+        this.setState({
+          zombies: array
+        })
+       
+
+        this.rect.current.fire('click', e);
+        }}>
+        <Stage width={550} height={550}>
+          <Layer>
+            <ColoredRect innerRef={this.rect} gp={this.props.parent} handleClick={this.toggleColor}/>
+            { this.state.zombies.map((zData, index) => {
+              console.log(zData)
+              return <Zombie key={index} id={zData.id} x={zData.x} y={zData.y} />
+            })}
+          </Layer>
+        </Stage>
+        <BackgroundParticles />
+      </div>
+    );
+  }
+}
+
+class Zombie extends React.Component {
   state = {
     color: "green"
   };
@@ -18,44 +95,19 @@ class ColoredRect extends React.Component {
 
 
   render() {
+    console.log('ye rects')
     return (
       <Rect
-        width={550}
-        height={550}
+        x={this.props.x}
+        y={this.props.y}
+        width={50}
+        height={50}
         opacity="0.5"
         fill={this.state.color}
         shadowBlur={5}
-        onClick={(e) => { this.setColor(); this.props.gp.setState({x: e.clientX, y: e.clientY})}}
+        onClick={(e) => { this.setColor() } }
         ref={this.props.innerRef}
       />
-    );
-  }
-}
-
-export default class Map extends Component {
-
-  constructor(props) {
-    super(props)
-    this.rect = React.createRef();
-  }
-
-  render() {
-    // Stage is a div container
-    // Layer is actual canvas element (so you may have several canvases in the stage)
-    // And then we have canvas shapes inside the Layer
-    console.log(this.rect)
-    return (
-      <div style={{ position: "relative", }} onClick={(e) => {
-        this.rect.current.fire('click', e);
-        console.log(e)
-        }}>
-        <Stage width={550} height={550}>
-          <Layer>
-            <ColoredRect innerRef={this.rect} gp={this.props.parent} handleClick={this.toggleColor}/>
-          </Layer>
-        </Stage>
-        <BackgroundParticles />
-      </div>
     );
   }
 }
